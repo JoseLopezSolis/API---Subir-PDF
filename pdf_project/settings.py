81beeb56c637f74822e2de0ea1d9a35a5dd8ed0d
joselopezsolis <pepeboss114@gmail.com>
2024-06-24 08:34:38 -0600

    Adding postgresql

--- pdf_project/settings.py
+++ pdf_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'pdf_api'
+    'pdf_api' 
 ]
 
 MIDDLEWARE = [
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'pdf_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
